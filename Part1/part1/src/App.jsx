0d3c49cbcc1381c44812a5e95751faa4d549aa67
KasperK <147533433+HARVAA@users.noreply.github.com>
2025-12-17 19:45:08 +0100

    1.12

--- Part1/part1/src/App.jsx
+++ Part1/part1/src/App.jsx
@@ -43,12 +43,36 @@ const Statistics = ({ good, neutral, bad }) => {
 
 const App = () => {
   komenda();
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const [selected, setSelected] = useState(0)
+
+  const nextAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+  }
+
   return (
     <div>
+      <h2>Anecdote of the day</h2>
+      <p>{anecdotes[selected]}</p>
+      <Button onClick={nextAnecdote} text="next anecdote" />
+
+      <h2>Give feedback</h2>
       <Button onClick={() => setGood(good + 1)} text="good" />
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
@@ -56,6 +80,6 @@ const App = () => {
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
-};
+}
 
 export default App
