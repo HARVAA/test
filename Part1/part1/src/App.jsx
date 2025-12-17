9e757b3490138630f661c7f26f09854d38da939b
KasperK <147533433+HARVAA@users.noreply.github.com>
2025-12-17 19:38:33 +0100

    1.11

--- Part1/part1/src/App.jsx
+++ Part1/part1/src/App.jsx
@@ -9,7 +9,12 @@ const Button = ({ onClick, text }) => {
 }
 
 const StatisticLine = ({ text, value }) => {
-  return <p>{text}: {value}</p>
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
 }
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -22,12 +27,16 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h2>Statistics</h2>
-      <StatisticLine text="Good" value={good} />
-      <StatisticLine text="Neutral" value={neutral} />
-      <StatisticLine text="Bad" value={bad} />
-      <StatisticLine text="Total feedback" value={total} />
-      <StatisticLine text="Average score" value={average.toFixed(2)} />
-      <StatisticLine text="Positive feedback" value={`${positivePercentage.toFixed(2)}%`} />
+      <table>
+        <tbody>
+          <StatisticLine text="Good" value={good} />
+          <StatisticLine text="Neutral" value={neutral} />
+          <StatisticLine text="Bad" value={bad} />
+          <StatisticLine text="Total" value={total} />
+          <StatisticLine text="Average" value={average.toFixed(2)} />
+          <StatisticLine text="Positive" value={`${positivePercentage.toFixed(2)}%`} />
+        </tbody>
+      </table>
     </div>
   )
 }
