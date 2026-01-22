468b4050ca4366bb6c1dee21c94b86f7ecf294ee
KasperK <harvaa.alt@gmail.com>
2026-01-22 13:13:21 +0100

    2.5

--- Part2/App.jsx
+++ Part2/App.jsx
@@ -1,39 +1,4 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
-};
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => (
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p><b>Number of exercises {total}</b></p>;
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+import Course from './Course';
 
 const App = () => {
   const courses = [
