bcd7956dd3412c175871c280485c44fbf657bf87
KasperK <harvaa.alt@gmail.com>
2026-01-22 13:05:07 +0100

    2.3

--- Part2/App.jsx
+++ Part2/App.jsx
@@ -21,7 +21,7 @@ const Content = ({ parts }) => {
 };
 
 const Total = ({ parts }) => {
-  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
   return <p><b>Number of exercises {total}</b></p>;
 };
 
