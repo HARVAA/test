951b41d1ac3cfad6a25a59a35faaa0056f687b9a
KasperK <harvaa.alt@gmail.com>
2026-02-26 13:17:51 +0100

    2.5 Declaring Course as a separate module

--- Part2/Course.jsx
+++ Part2/Course.jsx
@@ -1,38 +1,33 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
-};
+const Header = ({ course }) => (
+  <h2>{course.name}</h2>
+);
 
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-};
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
 
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => (
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+);
 
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p><b>Number of exercises {total}</b></p>;
-};
+const Total = ({ parts }) => (
+  <p>
+    <b>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</b>
+  </p>
+);
 
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+const Course = ({ course }) => (
+  <div>
+    <Header course={course} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+);
 
 export default Course;
