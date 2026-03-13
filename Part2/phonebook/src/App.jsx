f43b47e0c12884cdacc48d848941d6151956b834
KasperK <harvaa.alt@gmail.com>
2026-03-13 13:30:42 +0100

    2.12 and 2.13, [Work in progress]

---
+++ Part2/phonebook/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react'
+import Filter from './Service/Filter'
+import PersonForm from './Service/PersonForm'
+import Persons from './Service/Persons'
+import Service from './Service/Service'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    Service.getAll()
+      .then(data => {
+        console.log('Fetched persons:', data)
+        setPersons(Array.isArray(data) ? data : [])
+      })
+      .catch(error => {
+        console.error('Error fetching persons:', error)
+        setPersons([])
+      })
+  }, [])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    const newPerson = { name: newName, number: newNumber }
+    Service.create(newPerson).then(returnedPerson => {
+      setPersons(persons.concat(returnedPerson))
+      setNewName('')
+      setNewNumber('')
+    })
+  }
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      <h3>Add a new</h3>
+      <PersonForm
+        handleSubmit={handleSubmit}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h3>Numbers</h3>
+      <Persons personsToShow={personsToShow} />
+    </div>
+  )
+}
+
+export default App
